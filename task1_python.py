1db768a8195fa92cef0796963ab2ccf8f6ee8074
ramkrishnay2024-star <ramkrishnay2024@gmail.com>
2026-06-20 10:01:16 +0545

    python_assignment

---
+++ task1_python.py
@@ -0,0 +1,44 @@
+# Basic Arithematic and Type Identification
+
+# number = [12, 3.55, 4-6j]
+# print(type[0](number))
+# print(type(number[1]))
+# print(type(number[2]))
+
+# Arithmetic with Mixed Types
+
+# a = 5
+# b = 4.7
+
+# sum1 = a + b
+# difference = a - b
+# product = a * b
+# quotient = a/b
+
+# print(f"Sum1: {sum1}" )
+# print(type(sum1), "\n")
+
+# print(f"Difference: {difference} ")
+# print(type(difference), "\n")
+
+# print(f"Product: {product}")
+# print(type(product), "\n")
+
+# print(f"Quotient: {quotient}")
+# print(type(quotient))
+
+# Comparison With Mixed Types
+
+x = 10
+y = 7
+
+print(x>y) # Output is 'True' because x is greater than y.
+print(x<y) # Value of y is not greater than x so it's 'False'
+print(x==y) # Output is 'False' because both x,y value are not same.
+print(x!=y) # Result is 'True' because both are not equal to each other.
+
+# Boolean Variables
+
+satus = True
+
+
